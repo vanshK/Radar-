64f49ad30bc28bd7adfb5d4914339fd3366cfec6
vanshK <vkochar@sfu.ca>
2019-06-07 20:36:16 -0700

    Adding radar components - Distance and object detection

--- Radar.c
+++ Radar.c
@@ -52,3 +52,65 @@ int calculateDistance(){
   distance= duration*0.034/2;
   return distance;
 }
+
+void drawRadar() {
+  pushMatrix();
+  translate(960,1000); // moves the starting coordinats to new location
+  noFill();
+  strokeWeight(2);
+  stroke(98,245,31);
+  // draws the arc lines
+  arc(0,0,1800,1800,PI,TWO_PI);
+  arc(0,0,1400,1400,PI,TWO_PI);
+  arc(0,0,1000,1000,PI,TWO_PI);
+  arc(0,0,600,600,PI,TWO_PI);
+  // draws the angle lines
+  line(-960,0,960,0);
+  line(0,0,-960*cos(radians(30)),-960*sin(radians(30)));
+  line(0,0,-960*cos(radians(60)),-960*sin(radians(60)));
+  line(0,0,-960*cos(radians(90)),-960*sin(radians(90)));
+  line(0,0,-960*cos(radians(120)),-960*sin(radians(120)));
+  line(0,0,-960*cos(radians(150)),-960*sin(radians(150)));
+  line(-960*cos(radians(30)),0,960,0);
+  popMatrix();
+}
+
+void drawLine() {
+  pushMatrix();
+  strokeWeight(9);
+  stroke(30,250,60);
+  translate(960,1000); // moves the starting coordinats to new location
+  line(0,0,950*cos(radians(iAngle)),-950*sin(radians(iAngle))); // draws the line according to the angle
+  popMatrix();
+}
+
+void drawObject() {
+  pushMatrix();
+  translate(960,1000); // moves the starting coordinats to new location
+  strokeWeight(9);
+  stroke(255,10,10); // red color
+  pixsDistance = iDistance*22.5; // covers the distance from the sensor from cm to pixels
+  // limiting the range to 40 cms
+  if(iDistance<40){
+    // draws the object according to the angle and the distance
+  line(pixsDistance*cos(radians(iAngle)),-pixsDistance*sin(radians(iAngle)),950*cos(radians(iAngle)),-950*sin(radians(iAngle)));
+  }
+  popMatrix();
+}
+
+void draw() {
+  
+  fill(98,245,31);
+  textFont(orcFont);
+  // simulating motion blur and slow fade of the moving line
+  noStroke();
+  fill(0,4); 
+  rect(0, 0, width, 1010); 
+  
+  fill(98,245,31); // green color
+  // calls the functions for drawing the radar
+  drawRadar(); 
+  drawLine();
+  drawObject();
+  drawText();
+}
